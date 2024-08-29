9b27169c3269af970e48085fe5afe26bedcb0e63
louise <louise.easterbrook@gmail.com>
2024-08-29 20:54:40 +1000

    set up nav and home screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeScreenComponent } from './home/home-screen/home-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeScreenComponent },
+  //   { path: 'login', component: SecondComponent },
+];
